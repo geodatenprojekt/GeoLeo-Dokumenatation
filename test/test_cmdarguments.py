c7a642170ac53ef05fdf0528fb883074df676415
Valentin Hertel <Valentin.Hertel@fh-bielefel.de>
2019-05-20 18:15:39 +0200

    create Parameters Test class

---
+++ test/test_cmdarguments.py
@@ -0,0 +1,13 @@
+import unittest
+import sys
+import argparse
+from cmdarguments import cmdargs
+class Test_cmdarguments(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_Parameters_cadasterPath(self):
+        pass
+
+    def tearDown(self) -> None:
+        pass
