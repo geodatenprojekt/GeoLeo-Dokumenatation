05ba1c3232bcc1dacec45dd0a3ad49888dcc575c
Valentin Hertel <Valentin.Hertel@fh-bielefel.de>
2019-05-20 18:43:28 +0200

    test writeFile in PointCloudReader

--- test/test_PointCloudReader.py
+++ test/test_PointCloudReader.py
@@ -1,4 +1,6 @@
 import unittest
+import os
+import os.path
 from backend import PointCloudReader
 
 class Test_PointCloudReader(unittest.TestCase):
@@ -14,7 +16,11 @@ class Test_PointCloudReader(unittest.TestCase):
         with self.assertRaises(OSError):
             PointCloudReader.PointCloudFileIO("No File")
 
-
+    def test_writeFileToPath_writefile(self):
+        pointCloud = PointCloudReader.PointCloudFileIO("../backend/example_data/47078_575411_0011.laz")
+        pointCloud.writeFileToPath("../backend/example_data/test.las")
+        self.assertTrue(os.path.isfile("../backend/example_data/test.las"))
+        #os.remove("../backend/example_data/test.las")
 
     def tearDown(self) -> None:
         pass
