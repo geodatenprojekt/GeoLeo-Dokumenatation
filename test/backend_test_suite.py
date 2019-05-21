0ac3a6e855f27fdcb5185b9477846ab010357ffe
valentin235 <valentin.hertel@fh-bielefeld.de>
2019-05-21 14:08:09 +0200

    add util testcase in test suit

--- test/backend_test_suite.py
+++ test/backend_test_suite.py
@@ -3,6 +3,7 @@ import unittest
 # import backend test modules
 from test import test_PointCloudReader
 from test import test_cmdarguments
+from test import test_util
 
 #initialize the test suit
 loader = unittest.TestLoader()
@@ -11,6 +12,7 @@ suite = unittest.TestSuite()
 #add test to the test suite
 suite.addTest(loader.loadTestsFromModule(test_PointCloudReader))
 suite.addTest(loader.loadTestsFromModule(test_cmdarguments))
+suite.addTest(loader.loadTestsFromModule(test_util))
 
 #initialize a runner, pass it your suit and run it
 runner = unittest.TextTestRunner(verbosity=3)
